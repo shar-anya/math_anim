cb963156ba594e3b7bfcea46373b1e6b9457d534
Sharanya A <alice@localhost.localdomain>
2019-06-12 15:25:15 +0530

    Added fancy domain line animation with sin graph

--- just_testing.py
+++ just_testing.py
@@ -8,7 +8,12 @@ class TextTest(Scene):
 
 class HexTest(Scene):
     def construct(self):
-        hexagon = RegularPolygon(n = 5)
-        hexagon.round_corners(radius = 0.2)
-        self.play(ShowCreation(hexagon))
+        # hexagon = RegularPolygon(n = 5)
+        # hexagon.round_corners(radius = 0.2)
+        # self.play(ShowCreation(hexagon))
+
+        curvearrow = ArrowTip(color = WHITE)
+        curvearrow.shift(2*RIGHT)
+        curvearrow.scale(0.5)
+        self.play(ShowCreation(curvearrow))
         self.wait(2)
