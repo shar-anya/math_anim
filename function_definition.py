e82d628f9c85d8014bb5ef89032ff81dc79cf9f6
Sharanya A <alice@localhost.localdomain>
2019-06-18 10:02:18 +0530

    Cleaned Function_General code

--- function_definition.py
+++ function_definition.py
@@ -3,30 +3,32 @@ import numpy as np
 
 class Function_General(Scene):
     def construct(self):
+        #TITLE DISPLAY
         title = TextMobject("What is a function?")
         title.scale(1.3)
         title.to_edge(UP)
         self.play(ShowCreation(title))
         self.wait(1)
 
-        # INTRODUCTION
-        set1 = TextMobject(r"\{$\hspace{7pt}$\}")
-        set1.scale(4.5)
-        set1.shift(3.5*LEFT+0.5*DOWN)
-        set1text = TextMobject("some Set $A$")
-        set1text.shift(3.5*LEFT+2.3*DOWN)
+        #DOMAIN SET DISPLAY
+        domain_set = TextMobject(r"\{$\hspace{7pt}$\}")
+        domain_set.scale(4.5)
+        domain_set.shift(3.5*LEFT+0.5*DOWN)
+        domain_set_text = TextMobject("some Set $A$")
+        domain_set_text.shift(3.5*LEFT+2.3*DOWN)
 
-        circle1 = Circle(radius = 0.25, color = MAROON_C, fill_color = MAROON_C, fill_opacity = 1)
-        circle1.shift(3.5*LEFT+0.1*UP)
-        square1 = Square(side_length = 0.4, color = MAROON_C, fill_color = MAROON_C, fill_opacity = 1)
-        square1.shift(4*LEFT+0.7*DOWN)
-        pentagon1 = RegularPolygon(n = 5, side_length=0.2, color = MAROON_C, fill_color = MAROON_C, fill_opacity = 1)
-        pentagon1.shift(3.1*LEFT+1.2*DOWN)
-        pentagon1.scale(0.27)
+        domain = []
+        domain.append(Circle(radius = 0.25, color = MAROON_C, fill_color = MAROON_C, fill_opacity = 1) for i in range(3))
+        domain.append(Square(side_length = 0.4, color = MAROON_C, fill_color = MAROON_C, fill_opacity = 1))
+        domain.append(RegularPolygon(n = 5, side_length=0.2, color = MAROON_C, fill_color = MAROON_C, fill_opacity = 1))
+        domain[0].shift(3.5*LEFT+0.1*UP)
+        domain[1].shift(4*LEFT+0.7*DOWN)
+        domain[2].shift(3.1*LEFT+1.2*DOWN)
+        domain[2].scale(0.27)
 
-        self.play(ShowCreation(set1))
-        self.play(ShowCreation(circle1), ShowCreation(square1), ShowCreation(pentagon1))
-        self.play(ShowCreation(set1text))
+        domaingp = VGroup(domain[0], domain[1], domain[2])
+        self.play(ShowCreation(domaingp))
+        self.play(ShowCreation(domain_set_text))
         self.wait(1)
 
         function_notation = TexMobject("f(x) = y", tex_to_color_map={"x": MAROON_C, "y": BLUE_E})
@@ -37,45 +39,34 @@ class Function_General(Scene):
         self.play(ReplacementTransform(title, function_notation))
         self.wait(1)
 
-        circle2 = circle1.copy()
-        square2 = square1.copy()
-        pentagon2 = pentagon1.copy()
-        self.add(circle2, square2, pentagon2)
+        mid = [elem.copy() for elem in domain]
+        self.add(mid[0], mid[1], mid[2])
 
-        self.play(ApplyMethod(circle2.move_to, 0.55*LEFT+2.72*UP))
-        circle3 = circle2.copy()
-        circle3.scale(1.4)
-        circle3.shift(1.95*RIGHT)
-        circle3.set_color(BLUE_E)
-        self.play(FadeOut(circle2), FadeIn(circle3))
-        self.play(ApplyMethod(circle3.move_to, 3.5*RIGHT+0.1*UP))
-        self.wait(1)
+        midmove = [0.55*LEFT+2.72*UP, 0.55*LEFT+2.72*UP, 0.55*LEFT+2.76*UP]
+        rangeside = [1.95*RIGHT, 1.95*RIGHT+0.07*DOWN, 1.95*RIGHT+0.07*DOWN]
+        rangemove = [3.5*RIGHT+0.1*UP, 4.2*RIGHT+0.9*DOWN, 3*RIGHT+1*DOWN]
+        scale_list = (1.4, 1.6, 1.58)
+        range = []
 
-        self.play(ApplyMethod(square2.move_to, 0.55*LEFT+2.71*UP))
-        square3 = square2.copy()
-        square3.scale(1.6)
-        square3.shift(1.95*RIGHT+0.07*DOWN)
-        square3.set_color(BLUE_E)
-        self.play(FadeOut(square2), FadeIn(square3))
-        self.play(ApplyMethod(square3.move_to, 4.2*RIGHT+0.9*DOWN))
-        self.wait(1)
+        for i in range(3):
+            self.play(ApplyMethod(mid[i].move_to, midmove[i]))
+            range.append(mid[i])
+            range[i].scale(scale_list[i])
+            range[i].shift(rangeside[i])
+            range[i].set_color(BLUE_E)
+            self.play(FadeOut(mid[i]), FadeIn(range[i]))
+            self.play(ApplyMethod(range[i].move_to, rangemove[i]))
+            self.wait(1)
 
-        self.play(ApplyMethod(pentagon2.move_to, 0.55*LEFT+2.76*UP))
-        pentagon3 = pentagon2.copy()
-        pentagon3.scale(1.58)
-        pentagon3.shift(1.95*RIGHT+0.07*DOWN)
-        pentagon3.set_color(BLUE_E)
-        self.play(FadeOut(pentagon2), FadeIn(pentagon3))
-        self.play(ApplyMethod(pentagon3.move_to, 3*RIGHT+1*DOWN))
-        self.wait(1)
+        range_set = domain_set.copy()
+        range_set.move_to(3.5*RIGHT+0.5*DOWN)
+        self.play(ShowCreation(range_set))
 
-        set2 = TextMobject(r"\{$\hspace{7pt}$\}"); set2.scale(4.5); set2.move_to(3.5*RIGHT+0.5*DOWN)
-        self.play(ShowCreation(set2))
-
-        set2text= TextMobject("Range"); set2text.shift(3.5*RIGHT+2.3*DOWN)
-        self.play(ShowCreation(set2text))
-        set1_transform_text = TextMobject("Domain"); set1_transform_text.shift(3.5*LEFT+2.3*DOWN)
-        self.play(Transform(set1text, set1_transform_text))
+        range_set_text= TextMobject("Range")
+        range_set_text.shift(3.5*RIGHT+2.3*DOWN)
+        self.play(ShowCreation(range_set_text))
+        domain_set_transform_text = TextMobject("Domain"); domain_set_transform_text.shift(3.5*LEFT+2.3*DOWN)
+        self.play(Transform(domain_set_text, domain_set_transform_text))
 
         self.wait(2)
         self.clear()
@@ -123,8 +114,9 @@ class SingleVarFunction(GraphScene):
         #COMPACT_CODE_1
         self.play(ShowCreation(Dots[0]), ShowCreation(Dots[1]), ShowCreation(Dots[2]), ShowCreation(Dots[3]), ShowCreation(Dots[4]), ShowCreation(Dots[5]))
         self.play(ShowCreation(DotLabels[0]), ShowCreation(DotLabels[1]), ShowCreation(DotLabels[2]))
+        self.wait(1.5)
 
-        function_def = TexMobject(r"f(x) = sin(x)", tex_to_color_map={"f": YELLOW_D, "x": MAROON_C, "sin": BLUE_E})
+        function_def = TexMobject(r"f(x) = \sin(x)", tex_to_color_map={"f": YELLOW_D, "x": MAROON_C, "sin": BLUE_E})
         function_def.to_edge(UP)
         self.play(Transform(function_notation, function_def))
         #COMPACT_CODE_2
@@ -147,17 +139,21 @@ class SingleVarFunction(GraphScene):
             ArTips[i].shift(self.graph_origin+ scale_list[i]*4.7*X_TICKS_DISTANCE*LEFT)
 
         func_graph = self.get_graph(self.sin_graph, YELLOW_E)
-        self.play(ShowCreation(x_line), ShowCreation(ArTips[0]), ShowCreation(ArTips[1]))
+
+        self.play(Write(x_line), ShowCreation(ArTips[0]), ShowCreation(ArTips[1]))
         self.wait(1)
-        self.play(ShowCreation(func_graph), ShowCreation(y_line))
+        self.play(ShowCreation(func_graph), Write(y_line))
         #COMPACT_CODE_3
         self.play(ShowCreation(Dashes[0]), ShowCreation(Dashes[1]))
         self.wait(2)
         #COMPACT_CODE_4
         self.play(FadeOut(Dots[0]), FadeOut(Dots[1]),FadeOut(Dots[2]), FadeOut(Dots[3]),FadeOut(Dots[4]), FadeOut(Dots[5]),FadeOut(Dots[6]), FadeOut(Dots[7]),FadeOut(Dots[8]), FadeOut(DotLabels[0]), FadeOut(DotLabels[1]), FadeOut(DotLabels[2]) )
         self.wait(2)
-
-        # domain_text = TextTest(r"Domain: $\mathbb{R}$\\Range: $\mathbb{R}$")
+        domain_text = TextMobject(r"Domain: $\mathbb{R}$\\Range: $\mathbb{R}$", tex_to_color_map={"Domain": MAROON_C, "Range": BLUE_E})
+        domain_text.scale(0.7)
+        domain_text.shift(UP+2*LEFT)
+        self.play(Write(domain_text))
+        self.wait(3)
 
     def sin_graph(self,x):
         return np.sin(x)
--- just_testing.py
+++ just_testing.py
@@ -9,12 +9,13 @@ class TextTest(Scene):
 
 class HexTest(Scene):
     def construct(self):
-        # hexagon = RegularPolygon(n = 5)
+        # hexagon = RegularPolygon(n = 2)
         # hexagon.round_corners(radius = 0.2)
         # self.play(ShowCreation(hexagon))
+        circle = [Circle(radius = 0.25, color = MAROON_C, fill_color = MAROON_C, fill_opacity = 1) for i in range(3)]
 
-        curvearrow = ArrowTip(color = WHITE)
-        curvearrow.shift(2*RIGHT)
-        curvearrow.scale(0.5)
-        self.play(ShowCreation(curvearrow))
+        # curvearrow = ArrowTip(color = WHITE)
+        # curvearrow.shift(2*RIGHT)
+        # curvearrow.scale(0.5)
+        self.play(ShowCreation(circle[1]))
         self.wait(2)
