5b14436ac0ffc0f69827705d3e15ffa82b143460
Sh-Ac <sharanya.achut@gmail.com>
2019-10-25 15:00:57 +0530

    display initial equations

--- LinearAlgebra/matrix_rep.py
+++ LinearAlgebra/matrix_rep.py
@@ -4,10 +4,22 @@ from manimlib.imports import *
 
 class WriteDE(Scene):
     def construct(self):
-        FirstText = TextMobject("Consider a system of Linear Differential Equations")
-        FirstText.set_color_by_tex_to_color_map({"system": YELLOW_D, "Linear":GREEN,})
-        FirstText.scale(0.8)
-        FirstText.to_edge(TOP)
-
-        self.play(ShowCreation(FirstText))
+        self.show_text1()
         self.wait(2)
+
+    def show_text1(self):
+        self.text = [
+        TextMobject(r"Consider a system of Linear Differential Equations",
+        tex_to_color_map={"system": YELLOW_D, "Linear": GREEN_C}),
+        TexMobject("{\\frac{dx}{dt}} = 5x+ 1y}"),
+        TexMobject("{\\frac{dy}{dt}} = 3x+ 3y}"),
+        ]
+        # tex_to_color_map={"x": BLUE_C, "y": MAROON_C}),
+        text = self.text
+        self.text[0].shift(2*UP)
+        self.text[0].scale(0.7)
+        self.play(ShowCreation(self.text[0]))
+
+        text[2].shift(1.5*DOWN)
+        self.play(Write(text[1]))
+        self.play(Write(text[2]))
