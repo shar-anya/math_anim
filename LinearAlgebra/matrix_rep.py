3a977cc0092f05c4f971ea4d909d670c8e3675f6
Sh-Ac <sharanya.achut@gmail.com>
2019-10-26 14:48:54 +0530

    add code upto matrix form

--- LinearAlgebra/matrix_rep.py
+++ LinearAlgebra/matrix_rep.py
@@ -11,15 +11,50 @@ class WriteDE(Scene):
         self.text = [
         TextMobject(r"Consider a system of Linear Differential Equations",
         tex_to_color_map={"system": YELLOW_D, "Linear": GREEN_C}),
-        TexMobject("{\\frac{dx}{dt}} = 5x+ 1y}"),
-        TexMobject("{\\frac{dy}{dt}} = 3x+ 3y}"),
+        TexMobject("{\\frac{dx}{dt}}" , "{=}" , "{5}" , "{x}" , "{+}" , "{1}" , "{y}"),
+        TexMobject("{\\frac{dy}{dt}}" , "{=}" , "{3}" , "{x}" , "{+}" , "{3}" , "{y}"),
         ]
-        # tex_to_color_map={"x": BLUE_C, "y": MAROON_C}),
         text = self.text
-        self.text[0].shift(2*UP)
+        self.text[0].shift(1.5*UP)
         self.text[0].scale(0.7)
         self.play(ShowCreation(self.text[0]))
 
         text[2].shift(1.5*DOWN)
+        text[1].set_color_by_tex_to_color_map({"{x}": BLUE_C, "{y}": MAROON_C, "{5}":YELLOW_C , "{1}": YELLOW_C})
+        text[2].set_color_by_tex_to_color_map({"{x}": BLUE_C, "{y}": MAROON_C, "{3}":YELLOW_C , "{3}": YELLOW_C})
         self.play(Write(text[1]))
         self.play(Write(text[2]))
+
+        text.append(TextMobject("Two equations in two variables"))
+        text[-1].scale(0.7)
+        text[-1].shift(text[0].get_center())
+
+        self.wait(2)
+        self.play(Transform(text[0], text[-1]))
+        self.wait(2)
+
+        text.append(TextMobject("Matrix form"))
+        text[-1].scale(0.7)
+        text[-1].shift(text[0].get_center())
+
+        self.wait(1)
+        self.play(Transform(text[0], text[-1]))
+        self.wait(1)
+
+        text.append(self.text[1][3].copy()) # x
+        text.append(self.text[1][6].copy()) # y
+        text.append(self.text[2][3].copy()) # x
+        text.append(self.text[2][6].copy()) # y
+
+        text.append(self.text[1][2].copy()) # 5
+        text.append(self.text[1][5].copy()) # 1
+        text.append(self.text[2][2].copy()) # 3
+        text.append(self.text[2][5].copy()) # 3
+
+        self.add(*text[-8:])
+
+        text.append(TextMobject(r"$\begin{bmatrix} 5 & 1 \\ 3 & 3 \end{bmatrix}$")) # Matrix A
+        text[-1].to_edge(UP)
+        text.append(TextMobject(r"$\begin{bmatrix} x \\ y \end{bmatrix}$")) # Matrix [x \\ y]
+        text[-1].to_edge(UP)
+        text[-1].shift(1.2*RIGHT)
