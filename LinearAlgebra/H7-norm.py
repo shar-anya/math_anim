31d10107f9bbbf4bc4ebaa11b54f79613e937426
Sh-Ac <sharanya.achut@gmail.com>
2019-11-17 16:27:55 +0530

    create H7-norm.py

---
+++ LinearAlgebra/H7-norm.py
@@ -0,0 +1,5 @@
+# Animation clip demonstrating solution of points when they're near parallel and orthogonal
+# FILEID: H4
+from manimlib.imports import *
+import numpy as np
+from decimal import Decimal
