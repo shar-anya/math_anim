b9060b09170d30bb3450c06cc842f1429a740737
Sh-Ac <sharanya.achut@gmail.com>
2019-12-02 16:45:56 +0530

    change H9 to animate reflection across Y axis only

--- LinearAlgebra/H9-reflection_across_line.py
+++ LinearAlgebra/H9-reflection_across_line.py
@@ -1,8 +1,8 @@
-# Animation clip illustrating reflection of a vector v across line
+# Animation clip illustrating reflection of a vector v across line[k]
 # FILEID: H9
 from manimlib.imports import *
 
-class Reflection(LinearTransformationScene):
+class ReflectionY(LinearTransformationScene):
     CONFIG = {
         "include_background_plane": True,
         "include_foreground_plane": True,
@@ -18,49 +18,70 @@ class Reflection(LinearTransformationScene):
                                 }
         }
     def construct(self):
-        line = Line(start = FRAME_X_RADIUS*(LEFT+ 0.5*UP), end = FRAME_X_RADIUS*(RIGHT+ 0.5*DOWN), stroke_width = 5, color = WHITE)
-        self.play(FadeIn(line))
-
-        vector_coords = [-2,2]
-        line_slope = [2, -1]
-        I = np.array([[1,0],[0,1]])
-        v = np.array([[-1*line_slope[1]],[line_slope[0]]])
-        vt = np.transpose(v)
-        norm_v = np.power(v[0],2) + np.power(v[1],2)
-        calculated = I - (2*np.matmul(v,vt)/norm_v)
-        reflected = np.matmul(calculated,np.array([[vector_coords[0]],[vector_coords[1]]]))
-
+        line =  [Line(start = FRAME_X_RADIUS*(LEFT),
+                end = FRAME_X_RADIUS*(RIGHT),
+                stroke_width = 5, color = BLUE_B),
+                Line(start = FRAME_Y_RADIUS*(UP),
+                end = FRAME_Y_RADIUS*(DOWN),
+                stroke_width = 5, color = BLUE_C),
+                ]
+                # Line(start = FRAME_X_RADIUS*(LEFT+ 0.5*UP),
+                # end = FRAME_X_RADIUS*(RIGHT+ 0.5*DOWN),
+                # stroke_width = 5, color = BLUE_A),
+        vector_coords = [[3,1],
+                         [1.5,3],
+                         [0,4],
+                         [-2,4],
+                         [-4,0]]
+        line_slopes = [[1,0],[0,1]] #,[2, -1
         colors = [TEAL_D, MAROON_D]
-        vec = [
-                Vector(vector_coords, color = colors[0], stroke_width = 4),
-                Vector(reflected, color = colors[1], stroke_width = 4)
-            ]
 
-        arc =[
-            Arc(
-                start_angle=line.get_angle(),
-                angle= vec[0].get_angle()-line.get_angle(),
-                radius=0.4,
-                arc_center=ORIGIN,
-                stroke_width=3,
-                stroke_color=colors[0],
-                stroke_opacity=0.5,
-            ),
-            Arc(
-                start_angle=line.get_angle(),
-                angle= (vec[1].get_angle()-line.get_angle()),
-                radius=0.4,
-                arc_center=ORIGIN,
-                stroke_width=3,
-                stroke_color=colors[1],
-                stroke_opacity=0.5,
-            )]
-        arclabel = [((TexMobject(r"{\theta}").scale(0.7)).set_color(colors[i])).next_to(arc[i], RIGHT, SMALL_BUFF) for i in range(2)]
+        for k in range(1,2):
+            I = np.array([[1,0],[0,1]])
+            v = np.array([[-1*line_slopes[k][1]],[line_slopes[k][0]]])
+            vt = np.transpose(v)
+            norm_v = np.power(v[0],2) + np.power(v[1],2)
+            calculated = I - (2*np.matmul(v,vt)/norm_v)
+            self.play(FadeIn(line[k]))
+            self.wait(0.5)
+            # line_slope = line_slopes[i]
+            for i in range(len(vector_coords)):
+                reflected = np.matmul(calculated,np.array([[vector_coords[i][0]],[vector_coords[i][1]]]))
+                vec = [
+                        Vector(vector_coords[i], color = colors[0], stroke_width = 4),
+                        Vector(reflected, color = colors[1], stroke_width = 4)
+                    ]
 
-        self.play(ShowCreation(vec[0]))
-        self.play(ShowCreation(arc[0]))
-        self.play(ShowCreation(arclabel[0]))
-        self.play(ShowCreation(arc[1]))
-        self.play(ShowCreation(arclabel[1]))
-        self.play(ShowCreation(vec[1]))
-        self.wait(2)
+                arc = [Arc(
+                            start_angle=line[k].get_angle(),
+                            angle= vec[j].get_angle()-line[k].get_angle(),
+                            radius=0.4,
+                            arc_center=ORIGIN,
+                            stroke_width=3,
+                            stroke_color=colors[j],
+                            stroke_opacity=0.8,
+                        )
+                    for j in range(2)]
+                # if i is (len(vector_coords)-1):
+                if k is (1):
+                    arc[1] = Arc(
+                            start_angle=line[k].get_angle(),
+                            # angle= -(vec[1].get_angle()-line[1].get_angle()),
+                            angle= -vec[0].get_angle()-np.pi/2,
+                            radius=0.4,
+                            arc_center=ORIGIN,
+                            stroke_width=3,
+                            stroke_color=colors[1],
+                            stroke_opacity=0.8,
+                        )
+                direction = [RIGHT,LEFT]
+                arclabel = [((TexMobject(r"{\theta}").scale(0.7)).set_color(colors[i])).next_to(arc[i], direction[i], 1.4*SMALL_BUFF) for i in range(2)]
+                # arclabel[1].shift((0.2+0.07*i)*DOWN+(0.1+0.1*i)*LEFT)
+                self.play(ShowCreation(vec[0]))
+                self.play(ShowCreation(arc[0]),ShowCreation(arclabel[0]))
+                self.play(ShowCreation(arc[1]),ShowCreation(arclabel[1]))
+                self.play(ShowCreation(vec[1]))
+                self.wait(1)
+                self.play(FadeOut(VGroup(*vec,*arc,*arclabel)))
+            self.wait(1)
+            self.play(FadeOut(line[k]))
